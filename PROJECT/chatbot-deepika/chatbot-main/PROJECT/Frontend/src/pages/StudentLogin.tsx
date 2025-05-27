7441b6816aa7bb5132c0cf4f877601ebe5036f7c
AnfieldAura <addankipranav@gmail.com>
2025-05-27 23:03:59 +0530

    push

--- PROJECT/chatbot-deepika/chatbot-main/PROJECT/Frontend/src/pages/StudentLogin.tsx
+++ PROJECT/chatbot-deepika/chatbot-main/PROJECT/Frontend/src/pages/StudentLogin.tsx
@@ -13,7 +13,7 @@ const StudentLogin = () => {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5501/api/login", {
+      const response = await fetch("https://chatbott-six.vercel.app/student-login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ rollNumber, password }),
